45da853c3f6a44d8c58b8aa6a1e8bf8b5463b719
Leandro Femia <femia.leandro@gmail.com>
2019-02-24 23:29:34 -0300

    First commit

---
+++ Entities/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class User
+{
+    public int Id { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public DateTime Birthday { get; set; }
+}
